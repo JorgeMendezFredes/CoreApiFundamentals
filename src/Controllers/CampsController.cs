b8e599b4d444a1131c26f92af4010c7bcfde94a1
Jorge Méndez Fredes <jorge.mendez.fredes@gmail.com>
2019-06-10 17:29:50 -0400

    Optimización de Codigo

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -15,13 +15,12 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<CampModel[]>> Get()
         {
             try
             {
                 var results = await _repository.GetAllCampsAsync();
-                CampModel[] models = _mapper.Map<CampModel[]>(results);
-                return Ok(models);
+                return Ok(_mapper.Map<CampModel[]>(results));
             }
             catch (Exception e)
             {
